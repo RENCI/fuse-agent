d737cef0dc86befb1432de3f9956b0c80467efc3
Kimberly Robasky <krobasky@gmail.com>
2022-03-05 02:09:41 -0500

    added fuse-agent_network, provider_parameters, removed submit, started making models

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 import dateutil.parser
 import inspect
 import os
@@ -6,8 +6,10 @@ import os
 from fastapi import FastAPI, File, UploadFile, Form, Depends, HTTPException, Query
 from fastapi.logger import logger
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from typing import Type, Optional, List
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, HttpUrl
+from email_validator import validate_email, EmailNotValidError
+from typing import Type, Optional, List, Union
+from enum import Enum
 from starlette.responses import StreamingResponse
 
 from bson.json_util import dumps, loads
@@ -22,6 +24,149 @@ dictConfig(LogConfig().dict())
 logger = logging.getLogger("fuse-agent")
 
 
+def as_form(cls: Type[BaseModel]):
+    new_params = [
+        inspect.Parameter(
+            field.alias,
+            inspect.Parameter.POSITIONAL_ONLY,
+            default=(Form(field.default) if not field.required else Form(...)),
+        )
+        for field in cls.__fields__.values()
+    ]
+
+    async def _as_form(**data):
+        return cls(**data)
+
+    sig = inspect.signature(_as_form)
+    sig = sig.replace(parameters=new_params)
+    _as_form.__signature__ = sig
+    setattr(cls, "as_form", _as_form)
+    return cls
+
+class PCAParameters(BaseModel):
+    NumberPrincipalComponents: int = 3
+
+class CellularFunctionParameters(BaseModel):
+    SampleNumber: int = 32
+    Ref: str = "MT_recon_2_2_entrez.mat"
+    ThreshType: str = "local"
+    PercentileOrValue: str = "value"
+    Percentile: int = 25
+    Value: int = 5
+    LocalThresholdType: str = "minmaxmean"
+    PercentileLow: int = 25
+    PercentileHigh: int = 75
+    ValueLow: int = 5
+    ValueHigh: int = 5
+
+# xxx fit this to known data provider parameters
+@as_form
+class AnalysisParameters(BaseModel):
+    Union[PCAParameters,
+          CellularFunctionParameters]
+
+
+class Checksums(BaseModel):
+    checksum: str
+    type: str
+    
+class AccessURL(BaseModel):
+    url: AnyUrl=None
+    headers: str=None
+    
+class AccessMethods(BaseModel):
+    type: str=None
+    access_url: AccessURL = None
+    access_id: str=None
+    region: str=None
+    
+class Contents(BaseModel):
+    name: str=None
+    id: str=None
+    drs_uri: AnyUrl=None
+    contents: List[str] = []
+
+
+class DataType(str, Enum):
+    dataset_geneExpression = 'dataset-geneExpression'
+    results_PCA = 'results-PCA'
+    results_CellularFunction = 'results-cellularFunction'
+    # xxx to add more datatypes: expand this
+
+class JobStatus(str, Enum):
+    started='started'
+    failed='failed'
+    finished='finished'
+
+class Service(BaseModel):
+    id: str
+    title: str = None
+    URL: HttpUrl = None
+    
+
+class ProviderParameters(BaseModel):
+    submitter_id: EmailStr = Field(...,
+                                   title="email",
+                                   description="unique submitter id (email)")
+    service_id: str =        Field(...,
+                                   title="Provider service id",
+                                   description="id of service used to upload this object")
+    data_type: DataType =    Field(...,
+                                   title="Type of data",
+                                   descripton="informs the client how to solicit/render this data")
+    description: str =       Field(title="Description",
+                                   description="detailed description of this data (optional)")
+    group_id: str =          Field(title="External accession ID",
+                                   description="if sourced from a 3rd party, this is the accession ID on that db")
+    apikey: str =            Field(title="External apikey",
+                                   description="if sourced from a 3rd party, this is the apikey used for retrieval")
+    
+@as_form
+class ProviderObject(BaseModel): # xxx customize this code
+    parameters: ProviderParameters = Field(...,
+                                           title="Parameters",
+                                           description="parameters used with provider to create this object")
+    status: JobStatus =      Field(title="Job status",
+                                   description="object is incomplete until status is 'finished'")
+    # --
+    id: str = Field(...)
+    name: str
+    self_uri: AnyUrl =       Field(...,
+                                   title="Link to data",
+                                   descripton="use this link to retrieve the bytes when JobStatus is finished")
+    size: int =              Field(...,
+                                   title="Size of data",
+                                   descripton="size of the data, 'None' until JobStatus is finished")
+    created_time: datetime = Field(datetime.utcnow(),
+                                   title="Time this metadata record was created",
+                                   descripton="")
+    # xxx stopped here
+    updated_time: datetime = None
+    version: str="1.0"
+    mime_type: str="application/json"
+    checksums: List[Checksums] = []
+    access_methods: List[AccessMethods] = []
+    contents: List[Contents] = []
+    aliases: List[str] = []
+
+@as_form
+class ResultsObject(BaseModel): # xxx customize this code
+    source_data: ProviderObject = None
+    parameters: AnalysisParameters = None
+    results: ProviderObject = None
+
+class SubmitterStatus(str, Enum):
+    requested='requested'
+    approved='approved'
+    disabled='disabled'
+
+@as_form
+class Submitter(BaseModel):
+    object_id: str = None
+    submitter_id: EmailStr = None
+    created_time: datetime = None
+    status: SubmitterStatus = SubmitterStatus.requested
+    
 app = FastAPI()
 
 origins = [
@@ -51,6 +196,13 @@ import pathlib
 import json
 @app.get("/config", summary="Returns the config for the appliance")
 async def config():
+    '''
+    once you have the list of appliances, you can call each one separately to get the descriptoin of parameters to give to end-users;
+    for example, this to get the full schema forthe parameters required for submitting an object to be loaded by a data provider:
+    curl -X 'GET' '${provider_host}:${provider_port}/openapi.json' -H 'accept: application/json'   2> /dev/null |jq '.paths."/submit".post.parameters'
+    xxx this could also be done by the agent over a docker network if all containers are on the same machine; from inside any container:
+    curl -X 'GET' 'fuse-provider-upload:8000/openapi.json
+    '''
     config_path = pathlib.Path(__file__).parent / "config.json"
     with open(config_path) as f:
         return json.load(f)
@@ -88,14 +240,14 @@ async def add_submitter(submitter_id: str = Query(default=None, description="uni
         if entry.count() != 0:
             raise Exception("Submitter already added as: " + str(object_id)+", entries found = "+ str(entry.count()))
 
-        submitter_object = {
-            "object_id": object_id,
-            "submitter_id": submitter_id,
-            "created_time": datetime.datetime.utcnow(),
-            "status": "active"
-        }
+        submitter_object = Submitter(
+            object_id = object_id,
+            submitter_id = submitter_id,
+            created_time = datetime.utcnow(),
+            status = SubmitterStatus.approved)
+
         logger.info(msg=f"[add_submitter] submitter_object="+str(submitter_object))
-        mongo_submitters.insert(submitter_object)
+        mongo_submitters.insert(submitter_object.dict())
         logger.info(msg=f"[add_submitter] submitter added.")
 
         ret_val = {"submitter_id": submitter_id}
@@ -131,8 +283,8 @@ async def get_submitters(within_minutes: Optional[int] = Query(default=None, des
     try:
         if within_minutes != None:
             logger.info(msg=f"[submitters] get submitters created within the last %d minutes." % within_minutes)
-            until_time = datetime.datetime.utcnow()
-            within_minutes_time = datetime.timedelta(minutes=within_minutes)
+            until_time = datetime.utcnow()
+            within_minutes_time = timedelta(minutes=within_minutes)
             from_time = until_time - within_minutes_time
             search_object = {
                 "created_time": {
@@ -191,15 +343,47 @@ async def delete_submitter(submitter_id: str= Query(default=None, description="u
     }
     logger.info(msg=f"[delete_submitter] returning ("+str(ret)+")\n")
     return ret
+
+@app.get("/provider_parameters", summary="convenience function to get the submit parameters for a provider service")
+async def get_submit_parameters(service_id: str = Query(default="fuse-provider-upload", describe="loop through /providers to retrieve the submit parameters for each, allowing the end user to load in datasets as desired")):
+    '''
+    Only works for containers on the same 'fuse-agent_fuse' network as this agent; consequently, containers must be hosted on the same machine.
+    '''
+    import requests
+    response = requests.get("http://"+str(service_id)+":8000/openapi.json")# xxx won't work, needs to be on same docker network!
+    json_obj = response.json()
+    return json_obj['paths']['/submit']['post']['parameters'] # prints the string with 'source_name' key
+
+
+
     
-@app.post("/load", summary="load a dataset for analysis")
-async def load():
+# xxx can we build an enum class dynamically from the providers list?
+#@app.post("/load", summary="load a dataset for analysis")
+#async def load(parameters: ProviderParameters,
+#               files: Optional[List[UploadFile]] = File(...),
+#               ):
     '''
     params:
      - provider url
      - provider parameter values
     returns object_id
  '''
+
+@app.get("/objects/{object_id}", summary="get metadata for the object")
+async def get_object():
+    '''
+    gets object metadata, including link to object
+    Includes status and links to the input dataset, parameters, and dataset results
+    '''
+
+@app.get("/search/objects", summary="get all object metadata accessible for this submitter")
+async def get_results(submitter_id: Optional[str] = Query(default=None, description="unique identifier for the submitter (e.g., email)")):
+    '''
+    takes optional parameter "data_type=<data-type>" from [dataset-geneExpression, result-PCA, result-cellularFunction]
+    for dataset type objects, metadata would include provider url, provider object_id
+    for results type objects, metadata would include tool parameter values, tool url, provider url, provider object_id
+    '''
+
 
 @app.post("/analyze", summary="submit an analysis")
 async def analyze():
@@ -221,18 +405,3 @@ return the object_id
     '''
 
 
-@app.get("/search/objects", summary="get all object metadata accessible for this submitter")
-async def get_results(submitter_id: Optional[str] = Query(default=None, description="unique identifier for the submitter (e.g., email)")):
-    '''
-    takes optional parameter "data_type=<data-type>" from [dataset-geneExpression, result-PCA, result-cellularFunction]
-    for dataset type objects, metadata would include provider url, provider object_id
-    for results type objects, metadata would include tool parameter values, tool url, provider url, provider object_id
-    '''
-
-
-@app.get("/objects/{object_id}", summary="get metadata for the object")
-async def get_object():
-    '''
-    gets object metadata, including link to object
-    Includes status and links to the input dataset, parameters, and dataset results
-    '''
