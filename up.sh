d3b25244bd329ba17b8a479381eaa47a57647cd4
Jason Reilly <jdr0887@gmail.com>
2022-05-02 11:46:44 -0400

    fixing script

--- up.sh
+++ up.sh
@@ -4,14 +4,16 @@ set -a
 . .env
 set +a
 
-sudo chown -R ${USER} data
-if [ `docker network ls | awk '{print $2}'|grep -w fuse` ] ;
-then
+#sudo chown -R ${USER} data
+
+CMD=`docker network ls | awk '{print $2}' | grep -w fuse | head -1`
+RET=$CMD
+if [ "$RET" == "fuse" ]; then
     echo "found fuse, joining network";
 else
     echo "creating and joining fuse network";
     docker network create -d bridge fuse
 fi
-docker-compose -f docker-compose.yml up --build -V -d
+docker-compose -f docker-compose-dev.yml up --build -V -d
 
 
