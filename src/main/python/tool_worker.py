a9c1505779ff078e0a46f2f93ba17e2ca0aea2e0
Jason Reilly <jdr0887@gmail.com>
2022-05-19 14:07:33 -0400

    wrong queue

--- src/main/python/tool_worker.py
+++ src/main/python/tool_worker.py
@@ -1,8 +1,8 @@
 from rq import Worker, Queue, Connection
 
-from main import g_redis_connection, g_queue
+from main import g_redis_connection, tool_queue
 
 if __name__ == '__main__':
     with Connection(g_redis_connection):
-        worker = Worker(g_queue, connection=g_redis_connection)
+        worker = Worker(tool_queue, connection=g_redis_connection)
         worker.work()
